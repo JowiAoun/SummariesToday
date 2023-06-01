d3a220bfb321f6e91388cc01dc9e6481e8452444
Jowi <83415433+JowiAoun@users.noreply.github.com>
2023-06-01 18:29:12 -0400

    SVG stuff

--- frontend/src/Topbar.tsx
+++ frontend/src/Topbar.tsx
@@ -1,71 +1,12 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Topbar.css";
 
 function Topbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          #Book Name
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Share
-              </a>
-            </li>
-            <li className="nav-item">
-              <img src="assets/"/>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+    <div className="topbar">
+      <div role="button" className="button sidebar-button">
+        <img className="svg" src="./src/assets/icons/sidebar-button.svg" />
       </div>
-    </nav>
+    </div>
   );
 }
 

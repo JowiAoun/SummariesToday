9a5e22ddeb8297d347e7d21f2a36b497850a6477
Jowi <83415433+JowiAoun@users.noreply.github.com>
2023-06-02 21:03:19 -0400

    Added button div transition

--- frontend/src/Topbar.tsx
+++ frontend/src/Topbar.tsx
@@ -1,13 +1,19 @@
+import React from "react";
 import "./Topbar.css";
 
 function Topbar() {
   return (
     <div className="topbar">
-      <div role="button" className="button sidebar-button">
-        <img className="svg" src="./src/assets/icons/sidebar-button.svg" />
+      <div className="button-div sidebar-button" role="button">
+        <img
+          className="button-svg"
+          src="./src/assets/icons/sidebar-button.svg"
+          alt=""
+        />
       </div>
     </div>
   );
 }
 
 export default Topbar;
+

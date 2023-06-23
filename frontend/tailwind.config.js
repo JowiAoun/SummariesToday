7a895983837d480c31238aaffc9afd22ed0c62a6
Jowi <83415433+JowiAoun@users.noreply.github.com>
2023-06-22 23:41:44 -0400

    Using PostCSS to process TailwindCSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          DEFAULT: '#191919',
+          'strong': '#202020',            // Used for sidebar and main scrollbar
+          'potent': '#2c2c2c',            // Selected item background & Hover over item background
+          'white1': '#e8e6e3cf',          // Hovering over selected item background
+          'white2': '#d5d5d4',            // Item text
+          'white3': '#ffffffcf',          // Selected item text
+          'white4': '#ffffff08'           // Item text on mouse click
+        }
+      },
+    },
   },
   plugins: [],
 }
